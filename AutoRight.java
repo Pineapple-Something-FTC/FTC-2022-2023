37dfaccd6434c5d6ad1b954dc45c122e48f9da97
PineappleSomethingFTC <106415403+PineappleSomethingFTC@users.noreply.github.com>
2022-12-09 21:03:39 -0800

    USE THIS AUTONOMOUS (amongus susssys 69 bbaka basl)

---
+++ AutoRight.java
@@ -0,0 +1,423 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import static org.firstinspires.ftc.teamcode.PineappleOp.thing;
+
+import android.annotation.SuppressLint;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
+
+@Autonomous
+public class BlueBlueCones extends LinearOpMode {
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+    public static boolean right = false;
+    public static boolean left = true;
+    public static boolean forward = true;
+    public static boolean back = false;
+    int UP = 1;
+    int DOWN = -1;
+    int IN = 1;
+    int OUT = -1;
+    int NEUTRAL = 0;
+    final int speed = 1000;
+    // UNITS ARE METERS
+    double tagSize = 0.044;// Default value: 0.166
+
+    int LEFT = 1;
+    int MIDDLE = 2;
+    int RIGHT = 3;
+
+    AprilTagDetection tagOfInterest = null;
+    public static DcMotorEx frontLeft;
+    public static DcMotorEx backLeft;
+    public static DcMotorEx frontRight;
+    public static DcMotorEx backRight;
+    public static CRServo thing;
+    public static DcMotorEx g;
+
+    public static AnalogInput deeznuts;
+
+    @Override
+    public void runOpMode() {
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagSize, fx, fy, cx, cy);
+        frontLeft = hardwareMap.get(DcMotorEx.class, "motor1");
+        backLeft = hardwareMap.get(DcMotorEx.class, "motor2");
+        frontRight = hardwareMap.get(DcMotorEx.class, "motor3");
+        backRight = hardwareMap.get(DcMotorEx.class, "motor4");
+        g = hardwareMap.get(DcMotorEx.class, "g");
+        thing = hardwareMap.get(CRServo.class, "thing");
+        deeznuts = hardwareMap.get(AnalogInput.class, "deez2");
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+            }
+        });
+        telemetry.setMsTransmissionInterval(50);
+
+        /*
+         * The INIT-loop:
+         * This REPLACES waitForStart!
+         */
+        while (!isStarted() && !isStopRequested()) {
+            telemetry.addData("Potentiometer Voltage:", deeznuts.getVoltage());
+
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if(currentDetections.size() != 0) {
+                boolean tagFound = false;
+
+                for(AprilTagDetection tag : currentDetections) {
+                    //// CHANGE FROM ORIGINAL
+                    //if(tag.id == ID_TAG_OF_INTEREST)
+                    if(tag.id == LEFT || tag.id == MIDDLE || tag.id == RIGHT) {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if(tagFound) {
+                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                    tagToTelemetry(tagOfInterest);
+                } else {
+                    telemetry.addLine("Don't see tag of interest :(");
+
+                    if(tagOfInterest == null) {
+                        telemetry.addLine("(The tag has never been seen)");
+                    } else {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                        tagToTelemetry(tagOfInterest);
+                    }
+                }
+            } else {
+                telemetry.addLine("Don't see tag of interest :(");
+
+                if(tagOfInterest == null) {
+                    telemetry.addLine("(The tag has never been seen)");
+                } else {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest);
+                }
+            }
+            telemetry.update();
+            sleep(20);
+        }
+
+        /*
+         * The START command just came in: now work off the latest snapshot acquired
+         * during the init loop.
+         */
+
+        /* Update the telemetry */
+        if(tagOfInterest != null) {
+            telemetry.addLine("Tag snapshot:\n");
+            tagToTelemetry(tagOfInterest);
+            telemetry.update();
+        } else {
+            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+            telemetry.update();
+        }
+
+
+        // AUTONOMOUS CODE HERE:
+
+        resetEncoders();
+        intakeThing(IN, 850);
+        armThing(842, UP, speed, 1100);
+
+
+        move(200, forward, 1169,500);
+        turn(950, left, speed, 1670);
+        move(30, back, 1169, 200);
+        strafe(1779, right, speed, 2000);
+        move(248, forward,1169,430);
+        armThing(742, UP, speed, 600);
+        intakeThing(OUT, 200);
+        intakeThing(NEUTRAL, 50);
+        armThing(842, UP, speed, 700);
+        move(209, back, 1169, 430);
+
+        strafe(959, right, speed, 1520);
+        strafe(260, left, speed, 869);
+        turn(2069+4+2+6+9+3, right, speed, 2900);
+
+        armThing(274, UP, speed, 1480);
+        move(969+6+9+9+6, forward, 1169, 1369);
+        armThing(200, UP, speed, 300);
+        intakeThing(IN, 800);
+        armThing(369, UP, speed, 1300);
+        move(969, back, 1169, 1350);
+        armThing(842, UP, speed, 1150);
+        turn(1549,right, speed, 2150);
+        move(420, forward, 1169, 750);
+        armThing(742, UP, speed, 300);
+        intakeThing(OUT, 300);
+        intakeThing(NEUTRAL, 10);
+        armThing(842, UP, speed, 400);
+        move(420, back, 1409, 600);
+        turn(469+2+2, right, speed, 800);
+
+        if(tagOfInterest.id==LEFT) {
+            move(969, forward, speed, 1000);
+        }
+        else if (tagOfInterest.id==RIGHT) {
+            move(969, back, speed, 1000);
+        }
+        else {
+            move(69, forward, speed, 69);
+        }
+
+    }
+
+
+    public static void resetDriveEncoders() {
+        // Resets Encoders
+        frontLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+
+    public static void resetEncoders() {
+        // Resets Encoders
+        g.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    @SuppressLint("DefaultLocale")
+    void tagToTelemetry(AprilTagDetection detection) {
+        telemetry.addLine("\nDetected tag ID: " + detection.id);
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+    }
+
+    public void move(int ticks, boolean forwardOrBackward, int velocity, int sleep) {
+        resetDriveEncoders();
+        if (forwardOrBackward == true) {
+            // Drive forwards if `forwardOrBackward` is true
+            // Set target position
+            frontLeft.setTargetPosition(-ticks);
+            frontRight.setTargetPosition(-ticks);
+            backLeft.setTargetPosition(-ticks);
+            backRight.setTargetPosition(-ticks);
+
+            // Set mode
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // Set velocity
+            frontLeft.setVelocity(velocity);
+            frontRight.setVelocity(velocity);
+            backLeft.setVelocity(velocity);
+            backRight.setVelocity(velocity);
+        } else if (forwardOrBackward == false) {
+            // Drive backwards if `forwardOrBackward` is false
+            // Set target position
+            frontLeft.setTargetPosition(ticks);
+            frontRight.setTargetPosition(ticks);
+            backLeft.setTargetPosition(ticks);
+            backRight.setTargetPosition(ticks);
+
+            // Set mode
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // Set velocity
+            frontLeft.setVelocity(velocity);
+            frontRight.setVelocity(velocity);
+            backLeft.setVelocity(velocity);
+            backRight.setVelocity(velocity);
+        }
+
+        sleep(sleep);
+
+    }
+    public void turn(int degrees, boolean leftOrRight, int velocity, int sleep) {
+        resetDriveEncoders();
+        if(leftOrRight == true) {
+            // Drive left if `leftOrRight` is true
+            // Set target position
+            frontLeft.setTargetPosition(degrees);
+            frontRight.setTargetPosition(-degrees);
+            backLeft.setTargetPosition(degrees);
+            backRight.setTargetPosition(-degrees);
+
+            // Set mode
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // Set velocity
+            frontLeft.setVelocity(velocity);
+            frontRight.setVelocity(velocity);
+            backLeft.setVelocity(velocity);
+            backRight.setVelocity(velocity);
+        } else if (leftOrRight == false) {
+            // Drive right if `leftOrRight` is false
+            // Set target position
+            frontLeft.setTargetPosition(-degrees);
+            frontRight.setTargetPosition(degrees);
+            backLeft.setTargetPosition(-degrees);
+            backRight.setTargetPosition(degrees);
+
+            // Set mode
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // Set velocity
+            frontLeft.setVelocity(velocity);
+            frontRight.setVelocity(velocity);
+            backLeft.setVelocity(velocity);
+            backRight.setVelocity(velocity);
+        }
+        sleep(sleep);
+    }
+    public void strafe(int ticks, boolean leftOrRight, int velocity, int sleep) {
+        resetDriveEncoders();
+        if (leftOrRight == true) {
+            // Strafe left if `leftOrRight` is true
+            // Set target position
+            frontLeft.setTargetPosition(ticks);
+            frontRight.setTargetPosition(-ticks);
+            backLeft.setTargetPosition(-ticks);
+            backRight.setTargetPosition(ticks);
+
+            // Set mode
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // Set velocity
+            frontLeft.setVelocity(velocity);
+            frontRight.setVelocity(velocity);
+            backLeft.setVelocity(velocity);
+            backRight.setVelocity(velocity);
+        } else if (leftOrRight == false) {
+            // Strafe right if `leftOrRight` is false
+            // Set target position
+            frontLeft.setTargetPosition(-ticks);
+            frontRight.setTargetPosition(ticks);
+            backLeft.setTargetPosition(ticks);
+            backRight.setTargetPosition(-ticks);
+
+            // Set mode
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // Set velocity
+            frontLeft.setVelocity(velocity);
+            frontRight.setVelocity(velocity);
+            backLeft.setVelocity(velocity);
+            backRight.setVelocity(velocity);
+        }
+        sleep(sleep);
+    }
+    public void armThing(int ticks, int direction, int speed, int sleep) {
+
+        g.setTargetPosition(direction*ticks);
+        g.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        g.setVelocity(speed);
+        sleep(sleep);
+    }
+    public void intakeThing(int state, int sleep) {
+
+        thing.setPower(state);
+        sleep(sleep);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class intake implements Runnable
+    {
+        public void run()
+        {
+            thing.setPower(1);
+        }
+    }
+
